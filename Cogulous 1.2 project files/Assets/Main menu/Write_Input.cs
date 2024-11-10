33c23e90a7ecd5bd27f9d508c8c3be7f3e95a83c
Bill Shotgun <isen.cope@papamoacollege.school.nz>
2024-11-10 13:11:21 +1300

    updated game

--- Cogulous 1.2 project files/Assets/Main menu/Write_Input.cs
+++ Cogulous 1.2 project files/Assets/Main menu/Write_Input.cs
@@ -5,17 +5,18 @@ using static UnityEngine.UIElements.UxmlAttributeDescription;
 
 public class Write_Input : MonoBehaviour
 {
-    private string input;
+    private string input_Unprocessed;
 
     
     public void ReadStringInput(string s)
     {
-        input = s;
-        Debug.Log(input);
+        string input_Unprocessed = s + "       .";
+        Debug.Log(input_Unprocessed);
     }
 
     public void Savedata()
     {
+        string input = input_Unprocessed.Substring(0,5);
         PlayerPrefs.SetString("2", input);
     }
 }
--- Cogulous 1.2 project files/Assets/Player Scripts/Set_Score.cs
+++ Cogulous 1.2 project files/Assets/Player Scripts/Set_Score.cs
@@ -11,7 +11,7 @@ public class Set_Score : MonoBehaviour
     public int High_Score = 0;
     public Text Score_text;
     public Text High_Score_Text;
-    Dictionary<string, int> Top_Ten_Players = new Dictionary<string, int>();
+    public string Current_Player_Username = PlayerPrefs.GetString("2");
     
  
     // Start is called before the first frame update
@@ -19,18 +19,16 @@ public class Set_Score : MonoBehaviour
     {
         Score = PlayerPrefs.GetInt("1");
         Score_text.text = "Score: " + Score.ToString();
-        
-
-        Top_Ten_Players.Add("Test_Player",0);
-    }
-    private void Update()
-    {
         High_Score = PlayerPrefs.GetInt("3");
         High_Score_Text.text = "High Score: " + High_Score.ToString();
         if (Score > High_Score)
         {
             PlayerPrefs.SetInt("3", Score);
         }
+        
+    }
+    private void Update()
+    {
 
     }
 }
